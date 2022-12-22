cd65751ab15c3a6caeaf10634a3cede071acb901
Okkun555 <y.okuno.btm@gmail.com>
2022-12-22 22:49:28 +0900

    4-15:Creating State using useState

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import NewEntryForm from "./components/NewEntryForm";
 import DisplayBalance from "./components/DisplayBalance";
 import DisplayBalances from "./components/DisplayBalances";
 import EntryLine from "./components/EntryLine";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [entries, setEntries] = useState(initialEntries);
+
   return (
     <Container>
       <MainHeader title="Budget" />
@@ -17,14 +20,25 @@ function App() {
 
       <MainHeader title="History" type="h3" />
 
-      <EntryLine description="income" value="10.00" />
-      <EntryLine description="expense" value="100.00" isExpense />
-      <EntryLine description="income" value="20.00" />
+      {entries.map((entry, index) => (
+        <EntryLine
+          description={entry.description}
+          value={entry.value}
+          isExpense={entry.isExpense}
+          key={index}
+        />
+      ))}
 
       <MainHeader title="Add new transaction" type="h3" />
       <NewEntryForm />
     </Container>
   );
-}
+};
 
 export default App;
+
+const initialEntries = [
+  { description: "Work income", value: 1000, isExpense: false },
+  { description: "Water bill", value: 20, isExpense: true },
+  { description: "Rent", value: 300, isExpense: true },
+];
