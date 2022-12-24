b34f21f37d2977d95f5f8e6c1ffd8db4bf934a2a
Okkun555 <y.okuno.btm@gmail.com>
2022-12-24 09:08:35 +0900

    4-24:Making our components more reusable

--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Checkbox, Form, Segment } from "semantic-ui-react";
 import ButtonSaveOrCancel from "./ButtonSaveOrCancel";
+import EntryForm from "./EntryForm";
 
 const NewEntryForm = ({ addEntry }) => {
   const [description, setDescription] = useState("");
@@ -9,35 +10,14 @@ const NewEntryForm = ({ addEntry }) => {
 
   return (
     <Form unstackable>
-      <Form.Group>
-        <Form.Input
-          icon="tags"
-          width={12}
-          label="Description"
-          placeholder="New shinny thing"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <Form.Input
-          icon="dollar"
-          iconPosition="left"
-          width={4}
-          label="Value"
-          placeholder="100.00"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </Form.Group>
-
-      <Segment compact>
-        <Checkbox
-          toggle
-          label="is expense"
-          checked={isExpense}
-          onChange={() => setIsExpense((prevState) => !prevState)}
-        />
-      </Segment>
+      <EntryForm
+        description={description}
+        setDescription={setDescription}
+        value={value}
+        setValue={setValue}
+        isExpense={isExpense}
+        setIsExpense={setIsExpense}
+      />
 
       <ButtonSaveOrCancel
         addEntry={addEntry}
