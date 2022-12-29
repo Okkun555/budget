73c017fa3aee44a24545493a263ef92879d81c36
Okkun555 <y.okuno.btm@gmail.com>
2022-12-29 21:37:53 +0900

    7-49:Creating Modal Redux store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import entriesReducer from "./entries/reducers";
+import modalsReducer from "./modals/reducers";
 
 const configureStore = () => {
   return createStore(
     combineReducers({
       entries: entriesReducer,
+      modals: modalsReducer,
     }),
     composeWithDevTools()
   );
---
+++ src/store/modals/reducers.js
@@ -0,0 +1,12 @@
+const reducer = (state = {}, action) => {
+  switch (action) {
+    case "OPEN_EDIT_MODAL":
+      return;
+    case "CLOSE_EDIT_MODAL":
+      return;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
