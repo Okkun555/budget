8892e5aa671676aea5f817b00e2c32e69c274787
Okkun555 <y.okuno.btm@gmail.com>
2022-12-29 19:03:34 +0900

    7-48:update reducer action

--- src/store/entries/reducers.js
+++ src/store/entries/reducers.js
@@ -2,6 +2,15 @@ const reducer = (state = initialEntries, action) => {
   switch (action.type) {
     case "ADD_ENTRY":
       return state.concat({ ...action.payload });
+    case "UPDATE_ENTRY": {
+      return state.map((entry) => {
+        if (entry.id === action.payload.id) {
+          return { ...action.payload.entry };
+        }
+
+        return entry;
+      });
+    }
     case "REMOVE_ENTRY":
       return state.filter((entry) => entry.id !== action.payload.id);
     default:
