41d79d1d75b937fd90474a9db5c9cd8a5223f057
Okkun555 <y.okuno.btm@gmail.com>
2022-12-30 15:06:35 +0900

    9-61:Creating and running first saga

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,17 +2,20 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import entriesReducer from "./entries/reducers";
 import modalsReducer from "./modals/reducers";
 import createSagaMiddleware from "redux-saga";
+import { testSaga } from "../sagas/testSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const configureStore = () => {
-  return createStore(
+  const store = createStore(
     combineReducers({
       entries: entriesReducer,
       modals: modalsReducer,
     }),
     applyMiddleware(sagaMiddleware)
   );
+  sagaMiddleware.run(testSaga);
+  return store;
 };
 
 export default configureStore;
