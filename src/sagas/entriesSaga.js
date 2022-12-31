5b822452614ae48d30a6394edf4cba1add3ef30b
Okkun555 <y.okuno.btm@gmail.com>
2022-12-31 10:15:22 +0900

    12-72:saga using call

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,7 +1,10 @@
 import entriesTypes from "../store/entries/actions";
-import { take } from "redux-saga/effects";
+import { take, call } from "redux-saga/effects";
+import axios from "axios";
 
 export function* getAllEntries() {
   yield take(entriesTypes.GET_ENTRIES);
   console.log("I need to get the entries here");
+  const res = yield call(axios, "http://localhost:3005/entries");
+  console.log(res);
 }
