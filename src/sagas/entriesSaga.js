072768bf9a32dc8adc7e910e9c581cfb1e9e2c3e
Okkun555 <y.okuno.btm@gmail.com>
2023-01-01 23:47:43 +0900

    14--76:Implementing Saga Effect Fork in our app

--- src/sagas/entriesSaga.js
+++ src/sagas/entriesSaga.js
@@ -1,10 +1,22 @@
 import entriesTypes from "../store/entries/actions";
-import { take, call, put } from "redux-saga/effects";
+import { take, call, put, fork } from "redux-saga/effects";
 import axios from "axios";
 
 export function* getAllEntries() {
   yield take(entriesTypes.GET_ENTRIES);
-  console.log("I need to get the entries here");
   const result = yield call(axios, "http://localhost:3005/entries");
   yield put({ type: entriesTypes.GET_ENTRIES_SUCCESS, payload: result.data });
+}
+
+export function* getEntriesDetails(id) {
+  const { data } = yield call(axios, `http://localhost:3005/values/${id}`);
+  console.log(data);
+}
+
+export function* getAllEntriesDetails() {
+  const { payload } = yield take(entriesTypes.GET_ENTRIES_SUCCESS);
+  for (let index = 0; index < payload.length; index++) {
+    const entry = payload[index];
+    yield fork(getEntriesDetails, entry.id);
+  }
 }
